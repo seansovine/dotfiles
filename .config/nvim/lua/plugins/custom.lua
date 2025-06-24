ceea3fb28739b3ee64d626ea74d3937de8885ef5
Sean Sovine <sean.r.sovine@gmail.com>
2025-06-24 07:48:42 -0400

    Clean up neovim custom.lua

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -1,48 +1,46 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", lazy = false },
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "sainnhe/everforest" },
-  { "bluz71/vim-nightfly-colors" },
-  { "NLKNguyen/papercolor-theme" },
-  { "romainl/Apprentice" },
+	-- add color schemes
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "catppuccin/nvim", name = "catppuccin", lazy = false },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "sainnhe/everforest" },
+	{ "bluz71/vim-nightfly-colors" },
+	{ "NLKNguyen/papercolor-theme" },
+	{ "romainl/Apprentice" },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-dragon",
-    },
-  },
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "kanagawa-dragon",
+		},
+	},
 
-  -- add fuzzy finder
-  {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- or if using mini.icons/mini.nvim
-    -- dependencies = { "echasnovski/mini.icons" },
-    opts = {},
-  },
+	-- add fuzzy finder
+	{
+		"ibhagwan/fzf-lua",
+		-- optional, for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
 
-  -- setup treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "c", "cpp", "wgsl" },
-    },
-  },
+	-- setup treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "c", "cpp", "wgsl" },
+		},
+	},
 
-  -- setup file picker
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        hidden = true, -- for hidden files
-        ignored = true, -- for .gitignore files
-      },
-    },
-  },
+	-- setup file picker
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				hidden = true, -- for hidden files
+				ignored = true, -- for .gitignore files
+			},
+		},
+	},
 }
